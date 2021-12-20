3515f8d77d73a2b841cdb9cb8d6b0d9672be1900
Emilija Ristic <emilijaristic1124@gmail.com>
2021-12-20 21:10:37 +0100

    dodata ogranicenja

--- handler/add.php
+++ handler/add.php
@@ -2,8 +2,8 @@
 require "../dbBroker.php";
 require "../game.php";
 
-if (isset($_POST['gameName']) && isset($_POST['gameType']) 
-    && isset($_POST['gamePrice']) ) {
+if ($_POST['gameName']!= "" && $_POST['gameType']!=""
+    && $_POST['gamePrice']!="" ){
     Game::addGame($_POST['gameName'], $_POST['gameType'], $_POST['gamePrice'],$_POST['userId'], $connection);
 }
 
--- handler/update.php
+++ handler/update.php
@@ -3,7 +3,7 @@
 require "../dbBroker.php";
 require "../game.php";
 
-if (isset($_POST['gameName']) && isset($_POST['gameType'])
-     && isset($_POST['gamePrice'])) {
-   Game::editGame($_POST['gameId'], $_POST['gameName'], $_POST['gameType'], $_POST['gamePrice'], $connection);
+if ($_POST['gameName']!="" && $_POST['gameType']!=""
+     && $_POST['gamePrice']!="") {
+  Game::editGame($_POST['gameId'], $_POST['gameName'], $_POST['gameType'], $_POST['gamePrice'], $connection);
 }
